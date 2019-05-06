635a924268a95981c609c0ee0a5ae057c30e1a0c
tjhooper1 <tomh386@gmail.com>
2019-05-06 17:02:13 -0400

    added dataset ids

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 
 const wayPointContainer = document.querySelector('.wayPointContainer');
 const img = document.querySelector('.imgContainer');
+let counter = 0;
 
 img.addEventListener('click', function(event){
+    counter += 1;
     console.log(event);
-   const newWayPoint = document.createElement('div')
-   newWayPoint.classList.add('wayPointContainer');
-   const wayPoint = document.createElement('div');
-   wayPoint.classList.add('wayPoint');
-   newWayPoint.append(wayPoint);
-   newWayPoint.style.position = 'absolute';
-   newWayPoint.style.left = event.pageX - 10 + 'px';
-//    newWayPoint.style.right = 300
-   newWayPoint.style.top = event.pageY - 10 + 'px'
-//    newWayPoint.style.down = event.clientX + 'px'
-   document.body.append(newWayPoint);
+    const newWayPoint = document.createElement('div')
+    newWayPoint.classList.add('wayPointContainer');
+    newWayPoint.dataset.id = counter;
+    const wayPoint = document.createElement('div');
+    wayPoint.classList.add('wayPoint');
+    newWayPoint.append(wayPoint);
+    newWayPoint.style.position = 'absolute';
+    newWayPoint.style.left = event.pageX - 10 + 'px';
+    newWayPoint.style.top = event.pageY - 10 + 'px'
+    document.body.append(newWayPoint);
 })
