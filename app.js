7936950a6d92e8bc67d26e4e878c5b411e397123
tjhooper1 <tomh386@gmail.com>
2019-05-07 13:38:47 -0400

    adding modal boxes and capturing information

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 
+// MAKE MODAL FORM SAVE INFORMATION
+    //modal can save info to an object
+    //that object can then be used later to pull from in order to display dynamically into a box
+
+    // DISPLAY INFORMATION FOR EACH WAYPOINT WHEN MOUSE HOVERS OVER IT IN A FLOATING BOX
+// -------------------------------------------------------------------------
+
 
 const img = document.querySelector('.imgContainer');
 let counter = 0;
 
+//This event adds a wayPoint when clicked in the image
 $(img).on('click', function(e){
-    const htmlString = `<div data-id=${counter} class="wayPoint" style="left: ${e.pageX - 10}px; top: ${e.pageY - 10}px;"></div>`
+    const htmlString = `<div data-id=${counter} class="wayPoint" style="left: ${e.pageX - 10}px; top: ${e.pageY - 10}px;"></div>`;
     counter += 1;
     $(img).append(htmlString);
+    showForm();
     $('.wayPoint').on('click', function(){
-        $('.wayPoint').attr('data-target', '#myModal');
-        $('.wayPoint').attr('data-toggle', 'modal');
+        console.log($(this).data('id'));
+        showForm($(this).data('id'));
     });
 });
 
-    
+//Function that displays the modal form
+function showForm(id) {
+    $('.modalContainer').css('display', 'block');
+    $('#title').attr("placeholder", id); //TODO: change this later
 
+}    
 
-
-
-// $('.wayPoint').on('click', function(e){
-//     console.log(e);
-// })
+//Function that closes the modal form.
+$('#close').on('click', function(){
+    $('.modalContainer').css('display', 'none');
+    $('#title').attr("value", "");
+})
