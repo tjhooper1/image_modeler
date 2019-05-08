90eadf5c50e42bfbaee755d2f69189bb8458cfbd
tjhooper1 <tomh386@gmail.com>
2019-05-07 23:12:24 -0400

    added text box and hover effects

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // MAKE MODAL FORM SAVE INFORMATION
-    //modal can save info to an object
+    //modal can save info to an object ✅
     //that object can then be used later to pull from in order to display dynamically into a box
 
     // DISPLAY INFORMATION FOR EACH WAYPOINT WHEN MOUSE HOVERS OVER IT IN A FLOATING BOX
@@ -9,6 +9,7 @@
 
 const img = document.querySelector('.imgContainer');
 let counter = 0;
+let info = [];
 
 //This event adds a wayPoint when clicked in the image
 $(img).on('click', function(e){
@@ -20,12 +21,19 @@ $(img).on('click', function(e){
         console.log($(this).data('id'));
         showForm($(this).data('id'));
     });
+    $('.wayPoint').hover(function(){
+        if($('.modalContainer').css('display') == 'none'){
+            $('.textContainer').css('display', 'block');
+        }  
+    }, function(){
+        $('.textContainer').css('display', 'none');
+    })
 });
 
 //Function that displays the modal form
 function showForm(id) {
     $('.modalContainer').css('display', 'block');
-    $('#title').attr("placeholder", id); //TODO: change this later
+    $('#save').data("id"); //TODO: change this later
 
 }    
 
@@ -33,4 +41,16 @@ function showForm(id) {
 $('#close').on('click', function(){
     $('.modalContainer').css('display', 'none');
     $('#title').attr("value", "");
-})
+});
+
+//Function that saves the value from the modal to an object called info
+$('#save').on('click', function(id){
+    let object = {title: $('#title').val(), description: $('#description').val(), id: counter};
+    info.push(object);
+    $('#title').val('')
+    $('#description').val('');
+    console.log(info);
+    
+});
+
+
