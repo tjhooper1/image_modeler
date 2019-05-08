81fa7f03240a327224c1052db563fa97eb73e2b7
tjhooper1 <tomh386@gmail.com>
2019-05-08 16:23:09 -0400

    finished main functionality

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 // MAKE MODAL FORM SAVE INFORMATION
     //modal can save info to an object ✅
-    //that object can then be used later to pull from in order to display dynamically into a box
+    //that object can then be used later to pull from in order to display dynamically into a box ✅
 
-    // DISPLAY INFORMATION FOR EACH WAYPOINT WHEN MOUSE HOVERS OVER IT IN A FLOATING BOX
+    // DISPLAY INFORMATION FOR EACH WAYPOINT WHEN MOUSE HOVERS OVER IT IN A FLOATING BOX ✅
 // -------------------------------------------------------------------------
 
 
@@ -24,6 +24,8 @@ $(img).on('click', function(e){
     $('.wayPoint').hover(function(){
         if($('.modalContainer').css('display') == 'none'){
             $('.textContainer').css('display', 'block');
+            $('#textTitle').text(info[$(this).data('id')].title);
+            $('#textDescription').text(info[$(this).data('id')].description);
         }  
     }, function(){
         $('.textContainer').css('display', 'none');
@@ -40,7 +42,6 @@ function showForm(id) {
 //Function that closes the modal form.
 $('#close').on('click', function(){
     $('.modalContainer').css('display', 'none');
-    $('#title').attr("value", "");
 });
 
 //Function that saves the value from the modal to an object called info
@@ -50,7 +51,7 @@ $('#save').on('click', function(id){
     $('#title').val('')
     $('#description').val('');
     console.log(info);
-    
+    $('.modalContainer').css('display', 'none');
 });
 
 
